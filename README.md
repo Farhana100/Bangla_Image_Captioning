a95d8725530acf45e84fff2e8b7f7bb132bd4bdb
asifajrof <1705092@ugrad.cse.buet.ac.bd>
2023-02-18 00:39:14 +0600

    translate.py modified

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
 # ml_project_image_cap
+
+## usage
+
+- only refine the caption json file
+
+  ```bash
+  python translate.py --refine <path/to/raw/captions.json>
+  ```
+
+- refine and then translate the caption json file
+
+  ```bash
+  python translate.py --refine --translate <path/to/raw/captions.json>
+  ```
+
+- translate already refined caption json file
+
+  ```bash
+  python translate.py --translate <path/to/refined/captions.json>
+  ```
--- translate.py
+++ translate.py
@@ -1,18 +1,17 @@
 from googletrans import Translator as tr
 import json
 from tqdm import tqdm
+from pathlib import Path
+import argparse
 
-def import_json(train=True):
 
-    file_name = 'captions_train' if train else 'captions_val'
-
-    with open(file_name + '2017.json', 'r', encoding='utf-8') as f:
+def refine_json(filepath):
+    filepath = Path(filepath)
+    with open(filepath, 'r', encoding='utf-8') as f:
         data_raw = json.load(f)
-
 
     images = data_raw['images']
     annotations = data_raw['annotations']
-
 
     records = dict()
     for img in images:
@@ -24,17 +23,26 @@ def import_json(train=True):
 
         records[img['id']] = record
 
-    
     for ann in annotations:
         records[ann['image_id']]['captions'].append(ann['caption'])
 
-
     # json dump records
-    with open(file_name + '_refined.json', 'w', encoding='utf-8') as f:
+    refined_filepath = f'{filepath.stem}_refined.json'
+    refined_filepath = f'{filepath.parent}/{filepath.stem}_refined.json'
+    with open(refined_filepath, 'w', encoding='utf-8') as f:
         json.dump(records, f, indent=4)
 
-    return records
+    return refined_filepath
 
+
+def get_records(refined_path):
+    refined_path = Path(refined_path)
+
+    data_ref = None
+    with open(refined_path, 'r', encoding='utf-8') as f:
+        data_ref = json.load(f)
+
+    return data_ref
 
 
 def translate(text, src='en', dest='bn'):
@@ -46,31 +54,47 @@ def translate(text, src='en', dest='bn'):
 
 
 if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--refine', dest='refine',
+                        default=False, action='store_true')
+    parser.add_argument('--translate', dest='translate',
+                        default=False, action='store_true')
+    parser.add_argument('--path', dest='path', type=str,
+                        help='input json path')
+    parser.add_argument('--save_path', dest='save_path',
+                        default='.', type=str, help='output json path')
 
-    train = False
+    args = parser.parse_args()
+    # args =  parser.parse_args(args=['--refine', '--translate', '--path', '/content/annotations/captions_val2017.json'])
 
+    if args.refine:
+        print('refining...')
+        raw_path = args.path
+        refined_path = refine_json(filepath=raw_path)
+        print('done')
 
-    records = import_json(train=train)
+    if args.translate:
+        if args.refine:
+            # given path was raw
+            refined_path = refined_path
+        else:
+            # given path was refined
+            refined_path = args.path
 
-    img_ids = list(records.keys())
-    print(records[img_ids[0]])    
+        records = get_records(refined_path=refined_path)
+        translated_records = records.copy()
 
-    
-    translated_records = records.copy()
+        print('translating...')
+        for id in tqdm(translated_records):
+            caps = translated_records[id]['captions']
+            new_caps = translate(caps)
+            translated_records[id]['captions'] = new_caps
+        print('done')
 
-    # with tqdm 
-    for id in tqdm(translated_records):
-    # for id in translated_records:
-        caps = translated_records[id]['captions']
-        new_caps = translate(caps)
+        if args.save_path is None or args.save_path == "." or args.save_path == "" or args.save_path == " ":
+            save_path = f'{Path(refined_path).parent}/{Path(refined_path).stem}_translated.json'
+        else:
+            save_path = args.save_path
 
-        translated_records[id]['captions'] = new_caps
-
-
-    file_name = 'captions_train' if train else 'captions_val'
-
-    # json dump records
-    with open(file_name + '_translated.json', 'w', encoding='utf-8') as f:
-        json.dump(translated_records, f, indent=4)    
-
-
+        with open(save_path, 'w', encoding='utf-8') as f:
+            json.dump(translated_records, f, indent=4)
